a92b624995d88f7d26da0d4197b09a8f5b219f84
Marino Toscano <marino.toscano@gmail.com>
2026-05-14 18:28:53 +0200

    add belt item simulation

--- scripts/Belt.cs
+++ scripts/Belt.cs
@@ -1,4 +1,21 @@
-﻿public class Belt
+﻿using System;
+using Godot;
+
+public class Belt
 {
     public Item Item { get; set; }
+
+    public Direction Direction { get; set; }
+
+    public Vector2 GetNextPosition()
+    {
+        return Direction switch
+        {
+            Direction.Up => Vector2.Up,
+            Direction.Down => Vector2.Down,
+            Direction.Left => Vector2.Left,
+            Direction.Right => Vector2.Right,
+            _ => throw new NotImplementedException($"Direction not recognized"),
+        };
+    }
 }
--- scripts/SimulationManager.cs
+++ scripts/SimulationManager.cs
@@ -1,23 +1,26 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 public class SimulationManager : Node
 {
-    private const int TicksPerSecond = 5;
+    private const int TicksPerSecond = 2;
     private const float TickRate = 1.0f / TicksPerSecond;
     private float elapsedTime;
-    private readonly Dictionary<Vector2, Belt> belts = new Dictionary<Vector2, Belt>();
+    private readonly Dictionary<Vector2, Belt> belts = [];
     private TileMap tileMap;
+    private Node2D itemLayer;
 
     public override void _Ready()
     {
         tileMap = GetNode<TileMap>("/root/Game/TileMap");
+        itemLayer = GetNode<Node2D>("/root/Game/Items");
 
-        belts.Add(new Vector2(0, 0), new Belt());
-        belts.Add(new Vector2(1, 0), new Belt());
-        belts.Add(new Vector2(2, 0), new Belt());
-        belts.Add(new Vector2(3, 0), new Belt());
-        belts.Add(new Vector2(4, 0), new Belt());
+        belts.Add(new Vector2(0, 0), new Belt { Direction = Direction.Right, Item = Item.O });
+        belts.Add(new Vector2(1, 0), new Belt { Direction = Direction.Right });
+        belts.Add(new Vector2(2, 0), new Belt { Direction = Direction.Right });
+        belts.Add(new Vector2(3, 0), new Belt { Direction = Direction.Right });
+        belts.Add(new Vector2(4, 0), new Belt { Direction = Direction.Right });
 
         DrawBelts();
     }
@@ -35,27 +38,73 @@ public class SimulationManager : Node
 
     private void Tick()
     {
-        //foreach (var beltPair in belts)
-        //{
-        //    var position = beltPair.Key;
-        //    var belt = beltPair.Value;
+        DrawItems();
 
-        //    var tileId = 0;
+        foreach (var (position, belt) in belts.Reverse())
+        {
+            if (belt.Item == Item.Empty)
+            {
+                continue;
+            }
 
-        //    tileMap.SetCell((int)position.x, (int)position.y, tileId);
-        //}
+            var nextPosition = position + belt.GetNextPosition();
+            if (!belts.TryGetValue(nextPosition, out var nextBelt))
+            {
+                continue;
+            }
+
+            if (nextBelt.Item != Item.Empty)
+            {
+                continue;
+            }
+
+            nextBelt.Item = belt.Item;
+            belt.Item = Item.Empty;
+        }
     }
 
     private void DrawBelts()
     {
-        foreach (var beltPair in belts)
+        foreach (var (position, belt) in belts)
         {
-            var position = beltPair.Key;
-            var belt = beltPair.Value;
-
             var tileId = 0;
-
             tileMap.SetCell((int)position.x, (int)position.y, tileId);
         }
+    }
+
+    private void DrawItems()
+    {
+        foreach (var (position, belt) in belts)
+        {
+            if (belt.Item == Item.Empty)
+            {
+                continue;
+            }
+
+            Sprite sprite;
+            if (!itemLayer.HasNode(position.ToString()))
+            {
+                sprite = new Sprite { Name = position.ToString() };
+                itemLayer.AddChild(sprite);
+            }
+            else
+            {
+                sprite = itemLayer.GetNode<Sprite>(position.ToString());
+            }
+
+            sprite.Position = position * 8;
+            sprite.Texture = GetTextureForItem(belt.Item);
+            GD.Print("Texture position: " + sprite.Position);
+        }
+    }
+
+    private static Texture GetTextureForItem(Item item)
+    {
+        return item switch
+        {
+            Item.O => GD.Load<Texture>("res://assets/oxygen.png"),
+            Item.H => GD.Load<Texture>("res://assets/hydrogen.png"),
+            _ => null
+        };
     }
 }
